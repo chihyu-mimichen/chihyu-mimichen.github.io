caa017c29b5fa42fdee237607f34dcbb44c54b2f
Chih-Yu Chen <chihyu.chen@tum.de>
2025-02-22 06:51:01 +0100

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://chihyu-mimichen.github.io/',  // Make sure this is correct
+  site: 'https://chihyu-mimichen.github.io/', 
   integrations: [mdx(), sitemap(), tailwind()],
   outDir: "docs"  // Change output directory to /docs
 });
